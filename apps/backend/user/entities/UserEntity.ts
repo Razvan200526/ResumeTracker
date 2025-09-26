c2bb6ec23da2e66e34c350d51d9ba481f8862ded
RazvanAndrei200526 <doruippu@gmail.com>
2025-09-26 15:35:26 +0300

    folder page

--- apps/backend/user/entities/UserEntity.ts
+++ apps/backend/user/entities/UserEntity.ts
@@ -8,52 +8,52 @@ import {
   OneToOne,
   PrimaryColumn,
   UpdateDateColumn,
-} from 'typeorm';
-import type { UserAccountEntity, UserSessionEntity } from './index';
+} from "typeorm";
+import type { UserAccountEntity, UserSessionEntity } from "./index";
 @Entity({
-  name: 'users',
+  name: "users",
 })
-@Index(['email'], { unique: true })
+@Index(["email"], { unique: true })
 export class UserEntity {
   @PrimaryColumn({
-    name: 'id',
-    type: 'varchar',
+    name: "id",
+    type: "varchar",
   })
   id: string = crypto.randomUUID();
 
-  @Column({ name: 'email', type: 'varchar', length: 255 })
+  @Column({ name: "email", type: "varchar", length: 255 })
   email: string;
 
-  @Column({ name: 'password', type: 'varchar', length: 255 })
+  @Column({ name: "password", type: "varchar", length: 255 })
   password: string;
 
-  @Column({ name: 'name', type: 'varchar', length: 200 })
+  @Column({ name: "name", type: "varchar", length: 200 })
   name: string;
 
-  @Column({ name: 'first_name', type: 'varchar', length: 100 })
+  @Column({ name: "first_name", type: "varchar", length: 100 })
   firstName: string;
 
-  @Column({ name: 'last_name', type: 'varchar', length: 100 })
+  @Column({ name: "last_name", type: "varchar", length: 100 })
   lastName: string;
 
-  @OneToMany('UserSessionEntity', 'user')
+  @OneToMany("UserSessionEntity", "user")
   sessions: UserSessionEntity[];
 
-  @OneToOne('UserAccountEntity', 'user')
+  @OneToOne("UserAccountEntity", "user")
   account: UserAccountEntity;
 
-  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  @CreateDateColumn({ name: "created_at", type: "timestamptz" })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz', nullable: true })
+  @UpdateDateColumn({ name: "updated_at", type: "timestamptz", nullable: true })
   updatedAt?: Date | null;
 
-  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamptz', nullable: true })
+  @DeleteDateColumn({ name: "deleted_at", type: "timestamptz", nullable: true })
   deletedAt?: Date | null;
 
-  @Column({ name: 'locked_at', type: 'timestamptz', nullable: true })
+  @Column({ name: "locked_at", type: "timestamptz", nullable: true })
   lockedAt?: Date | null;
 
-  @Column({ name: 'blocked_at', type: 'timestamptz', nullable: true })
+  @Column({ name: "blocked_at", type: "timestamptz", nullable: true })
   blockedAt?: Date | null;
 }
